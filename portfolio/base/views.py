a1d573920db71173225f49373280879bde699b50
KartikAmin <aminjayanth11@gmail.com>
2024-01-03 22:44:13 +0530

    styled the page using CSS Bootstrap and other features

--- portfolio/base/views.py
+++ portfolio/base/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'base/home.html')
+
+def pdf_view(request):
+    with open('D:\Certificates\Full_Stack_Development.pdf', 'rb') as pdf:
+        response = HttpResponse(pdf.read(), content_type='application/pdf')
+        response['Content-Disposition'] = 'inline;filename=Full_Stack_Development.pdf'
+        return response
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -117,7 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/images/'
+MEDIA_URL = '/certificates/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
