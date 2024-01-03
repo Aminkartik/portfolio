ed5d0311f25d13ccdd1089a282f311635e0441d4
KartikAmin <aminjayanth11@gmail.com>
2024-01-04 01:33:48 +0530

    deleted cache files and added new image and contact icon

--- portfolio/base/urls.py
+++ portfolio/base/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('view-pdf/',views.pdf_view,name='pdf_view'),
+    # path('view-pdf/',views.pdf_view,name='pdf_view'),
 ]
--- portfolio/base/views.py
+++ portfolio/base/views.py
@@ -5,8 +5,8 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'base/home.html')
 
-def pdf_view(request):
-    with open('D:\Certificates\Full_Stack_Development.pdf', 'rb') as pdf:
-        response = HttpResponse(pdf.read(), content_type='application/pdf')
-        response['Content-Disposition'] = 'inline;filename=Full_Stack_Development.pdf'
-        return response
+# def pdf_view(request):
+#     with open('D:\Certificates\Full_Stack_Development.pdf', 'rb') as pdf:
+#         response = HttpResponse(pdf.read(), content_type='application/pdf')
+#         response['Content-Disposition'] = 'inline;filename=Full_Stack_Development.pdf'
+#         return response
