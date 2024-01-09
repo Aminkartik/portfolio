262def75628b50f5b357b055bfb7937d356aaa8f
KartikAmin <aminjayanth11@gmail.com>
2024-01-09 13:15:56 +0530

    updated static root file in settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -124,9 +124,9 @@ STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/images/'
 MEDIA_URL = '/certificates/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'staticfiles')
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
